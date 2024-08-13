3b0a553db50107c466ada5ada6ae05288b84ad16
kimkeongeun <118241290+kimkeongeun@users.noreply.github.com>
2024-08-13 12:04:21 +0900

    Create 10950_keongeun.c

---
+++ 8_week3/10950_bj/10950_keongeun.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+  int T;
+  int a,b;
+
+  scanf("%d", &T);
+
+  for(int i = 0; i<T; i++){
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a+b);
+  }
+
+    return 0;
+}
