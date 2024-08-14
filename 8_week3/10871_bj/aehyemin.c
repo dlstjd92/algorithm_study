789fca964eb435eca4eb760a68e4173458c1502d
aehyemin <hsy13467926@gmail.com>
2024-08-14 09:21:43 +0900

    10871_aehyemin

--- 8_week3/10871_bj/aehyemin.c
+++ 8_week3/10871_bj/aehyemin.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main () {
-    
+    int n,x,a,i;
+    scanf("%d %d", &n,&x);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    for(i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0; i<n; i++) {
+        if (arr[i] < x ) {
+            printf("%d ", arr[i]);
+        }
+    }
+    free(arr);
+    return 0;
 }
