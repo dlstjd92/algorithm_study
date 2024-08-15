5ce7768d878e6bf631a060c821bfd1789030e288
kimkeongeun <118241290+kimkeongeun@users.noreply.github.com>
2024-08-15 16:15:04 +0900

    2577

--- 8_week3/0815_2577_bj/2577_keongeun.c
+++ 8_week3/0815_2577_bj/2577_keongeun.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
 int main(void){
+
     int a,b,c;
     scanf("%d%d%d", &a, &b, &c);
     
+    int s = a*b*c;
+    int j = 1;
+    int V[10] = {0,};
+    int n = 10;
+
+
+    while((s/n)!=0){
+        V[(s%n)/(n/10)] += 1;
+        s -= (s%n);
+        n = n*10;
+    }
+
+    V[(s%n)/(n/10)] += 1;
+
+    for(int i=0; i<10; i++){
+        printf("%d\n", V[i]);
+    }
+
+    /*  방법 2
+    int a,b,c;
+    scanf("%d%d%d", &a, &b, &c);
     int s = a*b*c;
     int count;
 
@@ -12,13 +34,12 @@ int main(void){
         count = 0;
 
         while(j<s){
-            if(((s%(j*10))/j)==i){
+            if(((s%(j*10))/j)==i)
                 count++;
-            }
-
             j = j*10;
         }
-
         printf("%d\n", count);
     }
+    */
+    return 0;
 }
