cfd76934e8a19afe12bb74c44d16d6ef35b338e6
Alisser <gnlrjswjd@naver.com>
2024-08-17 14:16:10 +0900

    나는 햄복캅니다.

--- 8_week3/0817_9610_bj/9610_J_Hwi_mk2.c
+++ 8_week3/0817_9610_bj/9610_J_Hwi_mk2.c
@@ -2,25 +2,26 @@
 
 int main(void)
 {
-  int n, x[1000][2], q1 = 0, q2 = 0, q3 = 0, q4 = 0, AX = 0;
+  int n, x[1000][2];
+  int arr[5] = {0, 0, 0, 0, 0};
   scanf("%d", &n);
   for (int i = 0; i < n; i++)
   {
     scanf("%d %d", &x[i][0], &x[i][1]);
     if (x[i][0] > 0 && x[i][1] > 0) // 1사분면
-      q1++;
+      arr[0]++;
     else if (x[i][0] < 0 && x[i][1] > 0) // 2사분면
-      q2++;
+      arr[1]++;
     else if (x[i][0] < 0 && x[i][1] < 0) // 3사분면
-      q3++;
+      arr[2]++;
     else if (x[i][0] > 0 && x[i][1] < 0) // 4사분면
-      q4++;
+      arr[3]++;
     else
-      AX++;
+      arr[4]++;
   }
-  printf("Q1: %d\n", q1);
-  printf("Q2: %d\n", q2);
-  printf("Q3: %d\n", q3);
-  printf("Q4: %d\n", q4);
-  printf("AXIS: %d\n", AX);
+  printf("Q1: %d\n", arr[0]);
+  printf("Q2: %d\n", arr[1]);
+  printf("Q3: %d\n", arr[2]);
+  printf("Q4: %d\n", arr[3]);
+  printf("AXIS: %d\n", arr[4]);
 }
