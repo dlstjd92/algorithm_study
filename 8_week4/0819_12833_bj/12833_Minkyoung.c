f72e8756058a761a5c03aef50185d19bb312bc6e
Mikyoung00 <ubuntu@ip-172-31-42-89.ap-northeast-2.compute.internal>
2024-08-20 01:45:53 +0000

    [08.20]Minkyoung

---
+++ 8_week4/0819_12833_bj/12833_Minkyoung.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int A, B, C;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    if ( C % 2 == 0) 
+        printf("%d", A);
+    else 
+        printf("%d", A^B);
+    
+    return 0;
+}
