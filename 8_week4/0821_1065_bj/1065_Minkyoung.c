40f561e46a9bb88e9b7266fd433c01871894bd20
Mikyoung00 <ubuntu@ip-172-31-42-89.ap-northeast-2.compute.internal>
2024-08-23 05:16:55 +0000

    [08.23]Minkyoung

---
+++ 8_week4/0821_1065_bj/1065_Minkyoung.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int N, N_100, N_10, N_1, cnt;
+
+    scanf("%d", &N);
+
+    if (N < 100) cnt = N;
+    else{
+        cnt = 99;
+        for (int i = 100; i <= N; i++){
+            N_100 = (i % 1000) / 100;
+            N_10 = (i % 100) / 10;
+            N_1 = (i % 10);
+            if ((N_100 - N_10) == (N_10 - N_1)) 
+                    cnt++;
+        }
+        if(N == 1000) cnt--;
+    }
+    
+    printf("%d", cnt);
+    
+    return 0;  
+}
