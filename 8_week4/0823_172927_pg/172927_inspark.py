d148a16dfd497619f800a55bcac5972c33335061
박인성 <66993696+dlstjd92@users.noreply.github.com>
2024-08-24 16:09:05 +0900

    핀토스해야해서 미완성 올림 테스트케이스 15/20

---
+++ 8_week4/0823_172927_pg/172927_inspark.py
@@ -0,0 +1,60 @@
+def solution(picks, minerals):
+    answer = 0
+
+    # 일단 5개 묶음으로 끊어서 몇 구획인지 나누기
+
+    # sector = len(minerals) / 5 # 인트로 바꾸는 로직 필요함
+    if len(minerals) % 5 != 0:
+        sector = len(minerals) // 5 + 1
+    else:
+        sector = (int)(len(minerals) / 5)
+    # matrix = [[0].copy() for _ in range(6)]
+    matrix = [[None for _ in range(5+1)] for _ in range(sector)] # 끝은 풋터로 메타데이터값 넣을 예정
+
+    # print(matrix)
+
+    for i in range(sector - 1): # 제일 마지막은 5로 안나눠 떨어질 수 있어서 세그먼트에러 방지
+        for j in range(5):
+            # print(i,j)
+            matrix[i][j] = minerals.pop(0)  # 시간초과 되면 바꾸기 데크로
+
+        # 풋터에 피로도 지수 넣기
+        matrix[i][5] = matrix[i].count("diamond")*25 + matrix[i].count("iron")*5 + matrix[i].count("stone")
+
+
+    for i,j in enumerate(minerals):
+        matrix[sector-1][i] = j # 남은 부분 마저 넣기
+
+    matrix[sector-1][5] = matrix[sector-1].count("diamond") * 25 + matrix[sector-1].count("iron") * 5 + matrix[sector-1].count("stone")
+
+    # matrix.sort(key = lambda x:x[5])
+
+    print(matrix)
+
+    for i in range(3):
+        for j in range(picks[i]):
+            # print(answer)
+
+            if len(matrix) == 0:
+                return answer
+
+            temp = matrix.pop(0)
+            if i == 0:
+                answer += 5
+            elif i == 1:
+                answer += temp.count("diamond")*5 + temp.count("iron") + temp.count("stone")
+            elif i == 2:
+                answer += temp[5]
+
+    return answer
+
+picks = [1, 0, 1]
+minerals = ["stone", "stone", "stone", "stone", "stone", "iron", "iron", "iron", "iron", "iron", "diamond", "diamond"]
+
+print(solution(picks, minerals))
+
+
+
+
+
+
