7afceb7cb626e0b4505ec2834b34f80592307fa0
Alisser <gnlrjswjd@naver.com>
2024-09-05 15:20:34 +0900

    캐스퍼는 달리고 싶다...

--- 8_week4/0903_92341_pg/92341_J_Hwi.py
+++ 8_week4/0903_92341_pg/92341_J_Hwi.py
@@ -15,19 +15,21 @@ def solution(fees, records):
         else: # 아웃이면
             time = car_parking_time[car_num] # 입차시간 가져오기
             acctime = (int(car_time[:2]) - int(time[:2])) * 60 + (int(car_time[3:5]) - int(time[3:5]))
+            # 분 단위로 계산
 
-            del car_parking_time[car_num]
+            del car_parking_time[car_num] # 차가 나갔으니 기록 말소
 
+            # 기존 주차 시간에 이번 주차 시간 더하기
             car_parking_final[car_num] = car_parking_final.get(car_num, 0) + acctime
 
-    for car_num in car_parking_time:
+    for car_num in car_parking_time: # 아직 안나간 차들
         time = car_parking_time[car_num]
         car_time = "23:59"
         acctime = (int(car_time[:2]) - int(time[:2])) * 60 + (int(car_time[3:5]) - int(time[3:5]))
 
         car_parking_final[car_num] = car_parking_final.get(car_num, 0) + acctime
 
-    for car_num in car_parking_final:
+    for car_num in car_parking_final: # 주차비 계산
         acctime = car_parking_final[car_num]
 
         result = 0
