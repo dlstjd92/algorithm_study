b066fe8c24fc35110cc8a9908fd19cfdc981b8f5
Alisser <gnlrjswjd@naver.com>
2024-10-05 21:15:24 +0900

    너무 힘↗들 어↘요

--- 10_week1/14889_bj/14889_J_Hwi.py
+++ 10_week1/14889_bj/14889_J_Hwi.py
@@ -7,7 +7,7 @@ for i in range(N):
 
 visited = [0] * N
 
-min = 100
+min_A = 100
 
 # 능력치 재는 함수
 def ch_ABILITY(team):
@@ -18,3 +18,34 @@ def ch_ABILITY(team):
                 absility += human[i][j]
     return absility
 
+def backtrack(idx, count):
+    global min_A
+
+    # 절반 나눴으면 팀 완성~
+    if count == N // 2:
+        S_TEAM = []
+        L_TEAM = []
+
+        for i in range(N):
+            if visited[i]:
+                S_TEAM.append(i)
+            else:
+                L_TEAM.append(i)
+        
+        # 각 팀 능력치 계산
+        S_AVILITY = ch_ABILITY(S_TEAM)
+        L_AVILITY = ch_ABILITY(L_TEAM)
+
+        # 능력치 차 최소값
+        min_A = min(min_A, abs(S_AVILITY - L_AVILITY))
+        return
+
+    for i in range(idx, N):
+        if not visited[i]:
+            visited [i] = 1
+            backtrack(i + 1, count + 1)
+            visited[i] = 0
+
+backtrack(0,0)
+
+print(min_A)
