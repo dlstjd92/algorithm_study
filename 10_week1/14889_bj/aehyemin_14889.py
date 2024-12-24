eded31679d5cc5bdfd014433a5b01f07a63cf5ed
aehyemin <hsy13467926@gmail.com>
2024-12-25 00:25:56 +0900

    14235_aehyemin_bj

--- 10_week1/14889_bj/aehyemin_14889.py
+++ 10_week1/14889_bj/aehyemin_14889.py
@@ -1,1 +1,31 @@
-haha
+from itertools import combinations
+N = int(input())
+score = []
+for i in range(N):
+    score.append(list(map(int, input().split())))
+
+players = list(range(N))
+half = N // 2
+minV = 100000
+real_score = []
+
+for a_team in combinations(players, half):
+    b_team = list(set(players) - set(a_team))
+
+
+    a_score = 0
+    for i in a_team:
+        for j in a_team:
+            if i != j:
+                a_score += score[i][j]
+                
+    b_score = 0
+    for i in b_team:
+        for j in b_team:
+            if i != j:
+                b_score += score[i][j]
+    minV = min(minV, abs(a_score - b_score))
+    if minV == 0:
+        break
+        
+print(minV)
